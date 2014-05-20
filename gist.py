826a371a1cf5017b89fa15d54d9cf7ee9024b196
Terence Nip <terence.nip@gmail.com>
2014-05-20 01:51:01 -0400

    authed gisting is cool too + basic err handling

--- gist.py
+++ gist.py
@@ -3,9 +3,13 @@ from subprocess import call
 import os.path
 import json
 import requests
+from requests.auth import HTTPBasicAuth
 
 # Get the user's default editor
 EDITOR = os.environ.get("EDITOR", "vim")
+
+# Nabbed from here: https://github.com/settings/applications#personal-access-tokens
+GITHUB_PAC = "" 
 
 with tempfile.NamedTemporaryFile(suffix=".tmp") as temp:
     # Fire up vim
@@ -22,7 +26,14 @@ with tempfile.NamedTemporaryFile(suffix=".tmp") as temp:
     content = json.dumps(content)
 
     # Make the request and store response
-    g = requests.post("https://api.github.com/gists", data=content)
+    if len(GITHUB_PAC) > 0:
+        g = requests.post("https://api.github.com/gists", auth=HTTPBasicAuth(GITHUB_PAC, "x-oauth-basic"), data=content)
+    else:
+        g = requests.post("https://api.github.com/gists", data=content)
     r = g.json()
 
-    print r["html_url"]
+	# Handle errors plox
+    if 'message' in r.keys():
+        print "Error: " + r["message"]
+    else:
+        print r["html_url"]
